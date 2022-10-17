5079670626b5ff198373b5f1c67b07bb453498ec
David Cuartas <davidcuba91@gmail.com>
2022-10-18 00:09:32 +0200

    Add types of gentleman ts

---
+++ src/Gentleman/types.ts
@@ -0,0 +1,11 @@
+interface gentlemanList {
+  id: number;
+  name: string;
+  status: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+}
+export default gentlemanList;
